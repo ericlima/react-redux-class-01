26b04888fe49353bf5fd290b868b2750e08a1d3f
ericlima <eric.george.lima@gmail.com>
2020-03-29 21:33:34 +0100

    1 file modified, 5 deleted

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers:{
-        Authorization: 'Client-ID chrTkn0VYehpSo0Ivih8yK6ppHlfBuNnQo0vZrD0ZqU'
-      }
-})
