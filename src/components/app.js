1a4a701d970f5a108963f3211aef3e11156fb5c5
ericlima <eric.george.lima@gmail.com>
2020-03-31 08:52:11 +0100

    4 files modified

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React from "react";
 import SongList from "./SongList";
 
+
 class App extends React.Component {
   render() {
     return (
-      <div className="ui container" style={{ marginTop: "10px" }}>
-        <SongList />
+      <div className="ui container grid" >
+        <div className="ui row">
+          <div className="column eight wide">
+            <SongList />
+          </div>
+        </div>
       </div>
     );
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,10 +12,9 @@ const songsReducer = () => {
 };
 
 const selectedSongReducer = (selectedSong = null, action) => {
-  if (action === "SONG_SELECTED") {
+  if (action.type === "SONG_SELECTED") {
     return action.payload;
   }
-
   return selectedSong;
 };
 
